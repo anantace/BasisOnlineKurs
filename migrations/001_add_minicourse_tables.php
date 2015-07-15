68b66f4e5df27aea9112a432c11d1853427a7c66
asudau <sudau@elan-ev.de>
2015-07-15 12:34:06 +0200

    Zusaetzliche Tabs fuer TN auswaehlen
    Alle aktivierten Tabs können Ausgewählt werden
    - Auswahl (bisher auch für Autor mit drin)
    - Auswahl wird pro Kurs in DB gespeichert
    - Auswahl wird bisher bei der tatsächlichen Kurs-Darstellung für TN noch
    nicht berücksichtigt

--- migrations/001_add_minicourse_tables.php
+++ migrations/001_add_minicourse_tables.php
@@ -9,12 +9,10 @@ class AddMiniCourseTables extends DBMigration {
         $db = DBManager::get();
         $db->exec("CREATE  TABLE `minicourse` (
             `id` INT NOT NULL AUTO_INCREMENT,
-            `type` VARCHAR(64) NULL ,
-            `parent_id` INT NULL ,
+            `tab` VARCHAR(64) NULL ,
             `seminar_id` VARCHAR(32) NULL ,
             `title` VARCHAR(255) NULL ,
             `position` INT NULL DEFAULT 0 ,
-            `json_data` MEDIUMTEXT NULL COMMENT 'JSON' ,
             `chdate` INT NULL ,
             `mkdate` INT NULL ,
             PRIMARY KEY (`id`)
